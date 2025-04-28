1434cff871b85424e46a9f95cef5d2572f9d7b1a
Itzhak Levy <itzhaklevydev@gmail.com>
2025-04-28 13:46:20 +0300

    A clean slate before moving on to the next example

--- src/UseState/StateTutorial.js
+++ src/UseState/StateTutorial.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 const StateTutorial = () => {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleChange = (event) => {
-    const newValue = event.target.value;    // newValue temporarily holds what the user typed
-    setInputValue(newValue); // React updates inputValue to be equal to newValue
-  };
 
   return (
     <div>
-      <input placeholder="Enter your text" onChange={handleChange} />   {/* onChange is a prop that takes a function as an argument. The function is called when the input changes */}
-      {inputValue}
+      
     </div>
   );
 };
