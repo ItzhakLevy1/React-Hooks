057df0f8c2bdd1dfb87f5ac1d488180e0056abbb
Itzhak Levy <itzhaklevydev@gmail.com>
2025-04-28 15:11:50 +0300

    An example of toggling text and a counter using useState and WITH OUT useReducer

--- src/UseState/StateTutorial.js
+++ src/UseState/StateTutorial.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StateTutorial = () => {
+  const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(true);
+
+  const handleCounterAndText = () => {
+    // Using functional updates to ensure the latest state is used, even if updates are batched or delayed
+    setCount((count) => count + 1); 
+    setShowText((showText) => !showText);
+  };
 
   return (
     <div>
-      
+      <h1>{count}</h1>
+      <button onClick={handleCounterAndText}>Increment And Toggle Text</button>
+      {showText && <p>To be toggled</p>}
     </div>
   );
 };
