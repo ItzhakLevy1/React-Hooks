1f296fcea0e832eac68f89952e3d66084ea1bdb8
Itzhak Levy <itzhaklevydev@gmail.com>
2025-04-29 15:40:51 +0300

    Fetch data on button click with out any hooks at all

--- src/UseState/EffectTutorial.js
+++ src/UseState/EffectTutorial.js
@@ -1,26 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
 
 const EffectTutorial = () => {
-  const [data, setData] = useState("");
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
+  const fetchData = () => {
     axios
       .get("https://jsonplaceholder.typicode.com/comments")
       .then((response) => {
-        setData(response.data[0].email);
         console.log("API WAS CALLED");
         console.log(response.data);
       });
-  }, [count]); // Added count as a dependency to the useEffect so that it will re-run when count changes as the button is clicked
+  };
 
   return (
     <div>
       Hello World
-      <h1>{data}</h1>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Click Me</button>
+      <button onClick={fetchData}>Click Me</button>
     </div>
   );
 };
